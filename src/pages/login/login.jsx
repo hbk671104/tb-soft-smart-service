08aac31a3555ad93596eb1b65b1ef0e7efd7d524
hbk671104 <hbk671104@gmail.com>
2020-07-08 13:35:39 +0800

    sms login ui

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -21,7 +21,7 @@ export default class Login extends Component {
   login = async () => {
     try {
       Taro.showLoading({ title: '登录中...' })
-      const { code } = await wechatLogin()
+      const res = await wechatLogin()
       // to-do pass on the code
       Taro.reLaunch({
         url: '../index/index'
@@ -43,9 +43,47 @@ export default class Login extends Component {
       <View className='page login'>
         <View className='group'>
           <Image className='logo' src={require('../../assets/logo.png')} />
+          <View className='input-group'>
+            <View className='input-container'>
+              <Input
+                adjustPosition
+                autoFocus
+                className='input'
+                placeholderClass='input-placeholder'
+                placeholder='请输入手机号'
+                type='number'
+                confirmType='search'
+                onConfirm={this.onConfirm}
+              />
+            </View>
+            <View className='sms-input-container'>
+              <View className='input-container' style='flex:1;'>
+                <Input
+                  adjustPosition
+                  className='input'
+                  placeholderClass='input-placeholder'
+                  placeholder='请输入验证码'
+                  type='number'
+                  confirmType='search'
+                  onConfirm={this.onConfirm}
+                />
+              </View>
+              <View className='sms-button-container'>
+                <Button className='sms-button'>
+                  发送验证码
+                </Button>
+              </View>
+            </View>
+          </View>
         </View>
         <View className='button-group'>
-          <Button type='primary' onClick={this.onLoginClick}>登录</Button>
+          <Button
+            className='login-button'
+            hoverClass='login-button-hover'
+            onClick={this.onLoginClick}
+          >
+            登录
+          </Button>
         </View>
       </View>
     )
--- src/utils/init.js
+++ src/utils/init.js
@@ -12,7 +12,17 @@ export const initLeancloud = () => {
 
 export const wechatLogin = async () => {
   try {
-    const res = await Taro.login()
+    const { code } = await Taro.login()
+    const res = await AV.Cloud.run('code2Session', {
+      appid: WEAPP_ID,
+      secret: WEAPP_KEY,
+      js_code: code
+    })
+    // const user = await AV.User.loginWithAuthData({
+    //   openid,
+    //   access_token,
+    //   expires_in: 7200
+    // }, 'wechat')
     return Promise.resolve(res)
   } catch (error) {
     return Promise.reject(error)
