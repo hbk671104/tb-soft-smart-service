e6b7e1dd95cbd8d11a7ddfd4acc0f225ddfe481b
hbk671104 <hbk671104@gmail.com>
2020-07-21 18:21:04 +0800

    feature: temp dataform

--- src/pages/dataform/dataform.jsx
+++ src/pages/dataform/dataform.jsx
@@ -11,6 +11,8 @@ import {
 } from '@tarojs/components'
 import './dataform.scss'
 
+import dayjs from 'dayjs'
+import { AtNoticebar } from 'taro-ui'
 import {
   constructReportObject,
 } from '../../utils/leancloud'
@@ -20,6 +22,7 @@ const radio_color = 'rgb(186, 44, 40)'
 
 export default class DataForm extends Component {
   state = {
+    last_edit_at: null,
     data: {}
   }
 
@@ -27,7 +30,9 @@ export default class DataForm extends Component {
     this.currentUser = getCurrentUser()
   }
 
-  componentDidMount() { }
+  componentDidMount() {
+    this.getTempData()
+  }
 
   componentWillUnmount() { }
 
@@ -61,11 +66,8 @@ export default class DataForm extends Component {
     // add technician
     const { username } = this.currentUser
     data.technican = username
-    // add files
-    // const { upload_files } = this.state
-    // if (upload_files && upload_files.length > 0) {
-    //   value.related_files = upload_files
-    // }
+
+    // save it
     this.saveReport(data)
   }
 
@@ -75,6 +77,7 @@ export default class DataForm extends Component {
       Taro.showLoading({ title: '保存中...' })
       let result = await report.save()
       result = result.toJSON()
+      this.removeTempData()
       Taro.redirectTo({
         url: `../index/result/detail?id=${result.objectId}`
       })
@@ -161,11 +164,57 @@ export default class DataForm extends Component {
         ...this.state.data,
         [key]: value
       }
+    }, this.setTempData)
+  }
+
+  onNoticeClick = e => {
+    e.stopPropagation()
+    this.setState({
+      data: this.tempform
+    }, () => {
+      this.noticeBar.state.show = false
+      this.noticeBar.forceUpdate()
     })
+  }
+
+  setTempData = () => {
+    const { data } = this.state
+    try {
+      Taro.setStorageSync('temp_dataform', JSON.stringify({
+        last_edit_at: dayjs().format('lll'),
+        data
+      }))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  getTempData = () => {
+    try {
+      const dataform = Taro.getStorageSync('temp_dataform')
+      if (dataform) {
+        const { last_edit_at, data } = JSON.parse(dataform)
+        this.tempform = data
+        this.setState({
+          last_edit_at
+        })
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  removeTempData = () => {
+    try {
+      Taro.removeStorageSync('temp_dataform')
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   render() {
     const {
+      last_edit_at,
       data: {
         client_name,
         serviced_at,
@@ -179,143 +228,159 @@ export default class DataForm extends Component {
       }
     } = this.state
     return (
-      <View className='page data-form'>
-        <View className='item-container'>
-          <Text className='item-title'>客户名称：</Text>
-          <View className='input-container'>
-            <Input
-              value={client_name}
-              adjustPosition
-              className='input'
-              placeholderClass='input-placeholder'
-              placeholder='请输入客户名称'
-              onInput={this.onFieldInput('client_name')}
-            />
-          </View>
-        </View>
-        <View className='item-container'>
-          <Text className='item-title'>服务时间：</Text>
-          <Picker mode='date' onChange={this.onFieldInput('serviced_at')}>
+      <View>
+        {
+          !!last_edit_at &&
+          <AtNoticebar
+            ref={ref => {
+              this.noticeBar = ref
+            }}
+            close
+            single
+            showMore
+            moreText='继续编辑'
+            onGotoMore={this.onNoticeClick}
+          >
+            上次编辑于 {last_edit_at}
+          </AtNoticebar>
+        }
+        <View className='page data-form'>
+          <View className='item-container'>
+            <Text className='item-title'>客户名称：</Text>
             <View className='input-container'>
-              <Text
-                className={`input ${!serviced_at && 'input-placeholder'}`}
-              >
-                {!!serviced_at ? serviced_at : '请选择服务时间'}
-              </Text>
+              <Input
+                value={client_name}
+                adjustPosition
+                className='input'
+                placeholderClass='input-placeholder'
+                placeholder='请输入客户名称'
+                onInput={this.onFieldInput('client_name')}
+              />
             </View>
-          </Picker>
-        </View>
-        <View className='item-container'>
-          <Text className='item-title'>软件版本：</Text>
-          <View className='input-container'>
-            <Input
-              value={software_version}
-              adjustPosition
-              className='input'
-              placeholderClass='input-placeholder'
-              placeholder='请输入软件版本'
-              onInput={this.onFieldInput('software_version')}
-            />
           </View>
-        </View>
-        <View className='item-container'>
-          <Text className='item-title'>运行状态：</Text>
-          <View className='input-container'>
-            <Input
-              value={software_state}
-              adjustPosition
-              className='input'
-              placeholderClass='input-placeholder'
-              placeholder='请输入运行状态'
-              onInput={this.onFieldInput('software_state')}
-            />
+          <View className='item-container'>
+            <Text className='item-title'>服务时间：</Text>
+            <Picker mode='date' onChange={this.onFieldInput('serviced_at')}>
+              <View className='input-container'>
+                <Text
+                  className={`input ${!serviced_at && 'input-placeholder'}`}
+                >
+                  {!!serviced_at ? serviced_at : '请选择服务时间'}
+                </Text>
+              </View>
+            </Picker>
           </View>
-        </View>
-        <View className='item-container'>
-          <Text className='item-title'>运行类型：</Text>
-          <View className='radio-container'>
-            <RadioGroup onChange={this.onFieldInput('software_type')}>
-              <Radio
-                className='radio'
-                color={radio_color}
-                value='全量'
-                checked={software_type === '全量'}
-              >
-                全量
+          <View className='item-container'>
+            <Text className='item-title'>软件版本：</Text>
+            <View className='input-container'>
+              <Input
+                value={software_version}
+                adjustPosition
+                className='input'
+                placeholderClass='input-placeholder'
+                placeholder='请输入软件版本'
+                onInput={this.onFieldInput('software_version')}
+              />
+            </View>
+          </View>
+          <View className='item-container'>
+            <Text className='item-title'>运行状态：</Text>
+            <View className='input-container'>
+              <Input
+                value={software_state}
+                adjustPosition
+                className='input'
+                placeholderClass='input-placeholder'
+                placeholder='请输入运行状态'
+                onInput={this.onFieldInput('software_state')}
+              />
+            </View>
+          </View>
+          <View className='item-container'>
+            <Text className='item-title'>运行类型：</Text>
+            <View className='radio-container'>
+              <RadioGroup onChange={this.onFieldInput('software_type')}>
+                <Radio
+                  className='radio'
+                  color={radio_color}
+                  value='全量'
+                  checked={software_type === '全量'}
+                >
+                  全量
                 </Radio>
-              <Radio
-                className='radio'
-                style='margin-left: 12px;'
-                color={radio_color}
-                value='增量'
-                checked={software_type === '增量'}
-              >
-                增量
+                <Radio
+                  className='radio'
+                  style='margin-left: 12px;'
+                  color={radio_color}
+                  value='增量'
+                  checked={software_type === '增量'}
+                >
+                  增量
                 </Radio>
-            </RadioGroup>
+              </RadioGroup>
+            </View>
           </View>
-        </View>
-        <View className='item-container'>
-          <Text className='item-title'>报错代码信息：</Text>
-          <View className='input-container'>
-            <Textarea
-              value={error_detail}
-              adjustPosition
-              autoHeight
-              maxlength={-1}
-              className='area-input'
-              placeholderClass='input-placeholder'
-              placeholder='请输入报错代码信息'
-              onInput={this.onFieldInput('error_detail')}
-            />
+          <View className='item-container'>
+            <Text className='item-title'>报错代码信息：</Text>
+            <View className='input-container'>
+              <Textarea
+                value={error_detail}
+                adjustPosition
+                autoHeight
+                maxlength={-1}
+                className='area-input'
+                placeholderClass='input-placeholder'
+                placeholder='请输入报错代码信息'
+                onInput={this.onFieldInput('error_detail')}
+              />
+            </View>
           </View>
-        </View>
-        <View className='item-container'>
-          <Text className='item-title'>ORA 报错信息：</Text>
-          <View className='input-container'>
-            <Textarea
-              value={ora_error_detail}
-              adjustPosition
-              autoHeight
-              maxlength={-1}
-              className='area-input'
-              placeholderClass='input-placeholder'
-              placeholder='请输入 ORA 报错信息'
-              onInput={this.onFieldInput('ora_error_detail')}
-            />
+          <View className='item-container'>
+            <Text className='item-title'>ORA 报错信息：</Text>
+            <View className='input-container'>
+              <Textarea
+                value={ora_error_detail}
+                adjustPosition
+                autoHeight
+                maxlength={-1}
+                className='area-input'
+                placeholderClass='input-placeholder'
+                placeholder='请输入 ORA 报错信息'
+                onInput={this.onFieldInput('ora_error_detail')}
+              />
+            </View>
           </View>
-        </View>
-        <View className='item-container'>
-          <Text className='item-title'>故障原因：</Text>
-          <View className='input-container'>
-            <Textarea
-              value={error_cause_detail}
-              adjustPosition
-              autoHeight
-              maxlength={-1}
-              className='area-input'
-              placeholderClass='input-placeholder'
-              placeholder='请输入故障原因'
-              onInput={this.onFieldInput('error_cause_detail')}
-            />
+          <View className='item-container'>
+            <Text className='item-title'>故障原因：</Text>
+            <View className='input-container'>
+              <Textarea
+                value={error_cause_detail}
+                adjustPosition
+                autoHeight
+                maxlength={-1}
+                className='area-input'
+                placeholderClass='input-placeholder'
+                placeholder='请输入故障原因'
+                onInput={this.onFieldInput('error_cause_detail')}
+              />
+            </View>
           </View>
-        </View>
-        <View className='item-container'>
-          <Text className='item-title'>处理方案：</Text>
-          <View className='input-container'>
-            <Textarea
-              value={solution_detail}
-              adjustPosition
-              autoHeight
-              maxlength={-1}
-              className='area-input'
-              placeholderClass='input-placeholder'
-              placeholder='请输入处理方案'
-              onInput={this.onFieldInput('solution_detail')}
-            />
+          <View className='item-container'>
+            <Text className='item-title'>处理方案：</Text>
+            <View className='input-container'>
+              <Textarea
+                value={solution_detail}
+                adjustPosition
+                autoHeight
+                maxlength={-1}
+                className='area-input'
+                placeholderClass='input-placeholder'
+                placeholder='请输入处理方案'
+                onInput={this.onFieldInput('solution_detail')}
+              />
+            </View>
           </View>
-        </View>
-        {/* <View className='item-container'>
+          {/* <View className='item-container'>
             <View className='document-title-container'>
               <Text className='item-title'>相关文档：</Text>
               <View style='display:flex;align-items:center;'>
@@ -345,14 +410,15 @@ export default class DataForm extends Component {
               </View>
             )}
           </View> */}
-        <View className='submit-button-container'>
-          <Button
-            className='submit-button'
-            hoverClass='submit-button-hover'
-            onClick={this.onFormSubmit}
-          >
-            提交
+          <View className='submit-button-container'>
+            <Button
+              className='submit-button'
+              hoverClass='submit-button-hover'
+              onClick={this.onFormSubmit}
+            >
+              提交
             </Button>
+          </View>
         </View>
       </View>
     )
--- src/utils/init.js
+++ src/utils/init.js
@@ -1,5 +1,8 @@
 import AV from 'leancloud-storage/dist/av-weapp.js'
 import { LEANCLOUD_APP_ID, LEANCLOUD_APP_KEY } from './constant'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
 
 export const initLeanCloud = () => {
   AV.init({
@@ -10,3 +13,5 @@ export const initLeanCloud = () => {
 }
 
 initLeanCloud()
+dayjs.locale('zh-cn')
+dayjs.extend(localizedFormat)
