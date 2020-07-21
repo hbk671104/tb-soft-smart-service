cc001a5816d8d3a1233f5e39f8e51faa3326a339
hbk671104 <hbk671104@gmail.com>
2020-07-21 16:35:34 +0800

    get rid of <Form/> wrapper

--- src/pages/dataform/dataform.jsx
+++ src/pages/dataform/dataform.jsx
@@ -2,7 +2,6 @@ import Taro, { Component } from '@tarojs/taro'
 import {
   View,
   Text,
-  Form,
   Input,
   Picker,
   Radio,
@@ -21,8 +20,7 @@ const radio_color = 'rgb(186, 44, 40)'
 
 export default class DataForm extends Component {
   state = {
-    serviced_at: null,
-    // upload_files: null
+    data: {}
   }
 
   componentWillMount() {
@@ -41,16 +39,14 @@ export default class DataForm extends Component {
     navigationBarTitleText: '添加记录'
   }
 
-  onDateChange = ({ detail: { value } }) => {
-    this.setState({ serviced_at: value })
-  }
-
-  onFormSubmit = ({ detail: { value } }) => {
+  onFormSubmit = e => {
+    e.stopPropagation()
+    const { data } = this.state
     if (
-      !value.error_detail &&
-      !value.ora_error_detail &&
-      !value.error_cause_detail &&
-      !value.solution_detail
+      !data.error_detail &&
+      !data.ora_error_detail &&
+      !data.error_cause_detail &&
+      !data.solution_detail
     ) {
       Taro.showToast({
         title: '请填写报错信息/解决方案',
@@ -59,18 +55,18 @@ export default class DataForm extends Component {
       })
       return
     }
-    if (!value.software_version) {
-      value.software_version = '所有版本'
+    if (!data.software_version) {
+      data.software_version = '所有版本'
     }
     // add technician
     const { username } = this.currentUser
-    value.technican = username
+    data.technican = username
     // add files
     // const { upload_files } = this.state
     // if (upload_files && upload_files.length > 0) {
     //   value.related_files = upload_files
     // }
-    this.saveReport(value)
+    this.saveReport(data)
   }
 
   saveReport = async value => {
@@ -159,139 +155,167 @@ export default class DataForm extends Component {
   //   }
   // }
 
+  onFieldInput = key => ({ detail: { value } }) => {
+    this.setState({
+      data: {
+        ...this.state.data,
+        [key]: value
+      }
+    })
+  }
+
   render() {
-    const { serviced_at } = this.state
+    const {
+      data: {
+        client_name,
+        serviced_at,
+        software_version,
+        software_state,
+        software_type,
+        error_detail,
+        ora_error_detail,
+        error_cause_detail,
+        solution_detail
+      }
+    } = this.state
     return (
       <View className='page data-form'>
-        <Form onSubmit={this.onFormSubmit}>
-          <View className='item-container'>
-            <Text className='item-title'>客户名称：</Text>
+        <View className='item-container'>
+          <Text className='item-title'>客户名称：</Text>
+          <View className='input-container'>
+            <Input
+              value={client_name}
+              adjustPosition
+              className='input'
+              placeholderClass='input-placeholder'
+              placeholder='请输入客户名称'
+              onInput={this.onFieldInput('client_name')}
+            />
+          </View>
+        </View>
+        <View className='item-container'>
+          <Text className='item-title'>服务时间：</Text>
+          <Picker mode='date' onChange={this.onFieldInput('serviced_at')}>
             <View className='input-container'>
-              <Input
-                name='client_name'
-                adjustPosition
-                className='input'
-                placeholderClass='input-placeholder'
-                placeholder='请输入客户名称'
-              />
+              <Text
+                className={`input ${!serviced_at && 'input-placeholder'}`}
+              >
+                {!!serviced_at ? serviced_at : '请选择服务时间'}
+              </Text>
             </View>
+          </Picker>
+        </View>
+        <View className='item-container'>
+          <Text className='item-title'>软件版本：</Text>
+          <View className='input-container'>
+            <Input
+              value={software_version}
+              adjustPosition
+              className='input'
+              placeholderClass='input-placeholder'
+              placeholder='请输入软件版本'
+              onInput={this.onFieldInput('software_version')}
+            />
           </View>
-          <View className='item-container'>
-            <Text className='item-title'>服务时间：</Text>
-            <Picker mode='date' name='serviced_at' onChange={this.onDateChange}>
-              <View className='input-container'>
-                <Text
-                  className={`input ${!serviced_at && 'input-placeholder'}`}
-                >
-                  {!!serviced_at ? serviced_at : '请选择服务时间'}
-                </Text>
-              </View>
-            </Picker>
+        </View>
+        <View className='item-container'>
+          <Text className='item-title'>运行状态：</Text>
+          <View className='input-container'>
+            <Input
+              value={software_state}
+              adjustPosition
+              className='input'
+              placeholderClass='input-placeholder'
+              placeholder='请输入运行状态'
+              onInput={this.onFieldInput('software_state')}
+            />
           </View>
-          <View className='item-container'>
-            <Text className='item-title'>软件版本：</Text>
-            <View className='input-container'>
-              <Input
-                name='software_version'
-                adjustPosition
-                className='input'
-                placeholderClass='input-placeholder'
-                placeholder='请输入软件版本'
-              />
-            </View>
-          </View>
-          <View className='item-container'>
-            <Text className='item-title'>运行状态：</Text>
-            <View className='input-container'>
-              <Input
-                name='software_state'
-                adjustPosition
-                className='input'
-                placeholderClass='input-placeholder'
-                placeholder='请输入运行状态'
-              />
-            </View>
-          </View>
-          <View className='item-container'>
-            <Text className='item-title'>运行类型：</Text>
-            <View className='radio-container'>
-              <RadioGroup name='software_type'>
-                <Radio
-                  className='radio'
-                  color={radio_color}
-                  value='全量'
-                  checked
-                >
-                  全量
+        </View>
+        <View className='item-container'>
+          <Text className='item-title'>运行类型：</Text>
+          <View className='radio-container'>
+            <RadioGroup onChange={this.onFieldInput('software_type')}>
+              <Radio
+                className='radio'
+                color={radio_color}
+                value='全量'
+                checked={software_type === '全量'}
+              >
+                全量
                 </Radio>
-                <Radio
-                  className='radio'
-                  style='margin-left: 12px;'
-                  color={radio_color}
-                  value='增量'
-                >
-                  增量
+              <Radio
+                className='radio'
+                style='margin-left: 12px;'
+                color={radio_color}
+                value='增量'
+                checked={software_type === '增量'}
+              >
+                增量
                 </Radio>
-              </RadioGroup>
-            </View>
+            </RadioGroup>
           </View>
-          <View className='item-container'>
-            <Text className='item-title'>报错代码信息：</Text>
-            <View className='input-container'>
-              <Textarea
-                name='error_detail'
-                adjustPosition
-                autoHeight
-                maxlength={-1}
-                className='area-input'
-                placeholderClass='input-placeholder'
-                placeholder='请输入报错代码信息'
-              />
-            </View>
+        </View>
+        <View className='item-container'>
+          <Text className='item-title'>报错代码信息：</Text>
+          <View className='input-container'>
+            <Textarea
+              value={error_detail}
+              adjustPosition
+              autoHeight
+              maxlength={-1}
+              className='area-input'
+              placeholderClass='input-placeholder'
+              placeholder='请输入报错代码信息'
+              onInput={this.onFieldInput('error_detail')}
+            />
           </View>
-          <View className='item-container'>
-            <Text className='item-title'>ORA 报错信息：</Text>
-            <View className='input-container'>
-              <Textarea
-                name='ora_error_detail'
-                adjustPosition
-                autoHeight
-                maxlength={-1}
-                className='area-input'
-                placeholderClass='input-placeholder'
-                placeholder='请输入 ORA 报错信息'
-              />
-            </View>
+        </View>
+        <View className='item-container'>
+          <Text className='item-title'>ORA 报错信息：</Text>
+          <View className='input-container'>
+            <Textarea
+              value={ora_error_detail}
+              adjustPosition
+              autoHeight
+              maxlength={-1}
+              className='area-input'
+              placeholderClass='input-placeholder'
+              placeholder='请输入 ORA 报错信息'
+              onInput={this.onFieldInput('ora_error_detail')}
+            />
           </View>
-          <View className='item-container'>
-            <Text className='item-title'>故障原因：</Text>
-            <View className='input-container'>
-              <Textarea
-                name='error_cause_detail'
-                adjustPosition
-                autoHeight
-                maxlength={-1}
-                className='area-input'
-                placeholderClass='input-placeholder'
-                placeholder='请输入故障原因'
-              />
-            </View>
+        </View>
+        <View className='item-container'>
+          <Text className='item-title'>故障原因：</Text>
+          <View className='input-container'>
+            <Textarea
+              value={error_cause_detail}
+              adjustPosition
+              autoHeight
+              maxlength={-1}
+              className='area-input'
+              placeholderClass='input-placeholder'
+              placeholder='请输入故障原因'
+              onInput={this.onFieldInput('error_cause_detail')}
+            />
           </View>
-          <View className='item-container'>
-            <Text className='item-title'>处理方案：</Text>
-            <View className='input-container'>
-              <Textarea
-                name='solution_detail'
-                adjustPosition
-                autoHeight
-                maxlength={-1}
-                className='area-input'
-                placeholderClass='input-placeholder'
-                placeholder='请输入处理方案'
-              />
-            </View>
+        </View>
+        <View className='item-container'>
+          <Text className='item-title'>处理方案：</Text>
+          <View className='input-container'>
+            <Textarea
+              value={solution_detail}
+              adjustPosition
+              autoHeight
+              maxlength={-1}
+              className='area-input'
+              placeholderClass='input-placeholder'
+              placeholder='请输入处理方案'
+              onInput={this.onFieldInput('solution_detail')}
+            />
           </View>
-          {/* <View className='item-container'>
+        </View>
+        {/* <View className='item-container'>
             <View className='document-title-container'>
               <Text className='item-title'>相关文档：</Text>
               <View style='display:flex;align-items:center;'>
@@ -321,16 +345,15 @@ export default class DataForm extends Component {
               </View>
             )}
           </View> */}
-          <View className='submit-button-container'>
-            <Button
-              className='submit-button'
-              hoverClass='submit-button-hover'
-              formType='submit'
-            >
-              提交
+        <View className='submit-button-container'>
+          <Button
+            className='submit-button'
+            hoverClass='submit-button-hover'
+            onClick={this.onFormSubmit}
+          >
+            提交
             </Button>
-          </View>
-        </Form>
+        </View>
       </View>
     )
   }
